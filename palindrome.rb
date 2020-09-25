18bcc7fcbb84c5b3df9f93eaf0feb48560fd2caa
James Francis <james.franics@madetech.com>
2020-09-25 10:19:59 +0100

    adding gem files

--- palindrome.rb
+++ palindrome.rb
@@ -1,5 +1,59 @@
-def palindrome?(string)
-    processed_content = string.downcase
-    processed_content == processed_content.reverse
+
+
+module Palindrome
+    def palindrome?
+        processed_content == processed_content.reverse
+    end
+
+    private
+        def processed_content
+            self.to_s.downcase
+        end
+    
+    
 end
+
+class String
+    
+    include Palindrome
+
+end
+
+class Integer
+    include Palindrome
+end
+
+
+puts "Racecar".palindrome?
+
+
+
+
+
+
+
+
+
+
+# class TranslatedPhrase < Phrase
+#     attr_accessor :translation
+#     def initialize(content, translation)
+#         super(content)
+#         @translation = translation
+#     end
+
+#     def processed_content
+#         self.translation.downcase
+#     end
+# end
+
+# frase = TranslatedPhrase.new("recognize", "reconocer")
+# puts frase.palindrome?
+# puts frase.louder
+
+
+# phrase = Phrase.new("Racecar")
+# puts phrase.palindrome?
+
+
 
